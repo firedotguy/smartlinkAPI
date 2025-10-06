db14b33b7e97f1537f678fbc803dccce98eff0fc
TimurNasirov <nta16022013@gmail.com>
2025-10-06 14:06:17 +0300

    fix(ont): remove extra optical info call (#17)

--- ont.py
+++ ont.py
@@ -59,8 +59,8 @@ def search_ont(sn: str, host: str) -> tuple[dict, str | None] | None:
         sleep(0.1)
         clear_buffer(channel)
 
-        channel.send(bytes(f"display ont optical-info {ont_info['interface']['port']} {ont_info['ont_id']}\n", 'utf-8'))
         if ont_info.get('online'):
+            channel.send(bytes(f"display ont optical-info {ont_info['interface']['port']} {ont_info['ont_id']}\n", 'utf-8'))
             optical_info = parse_optical_info(read_output(channel))
             ont_info['optical'] = optical_info
 
@@ -79,6 +79,7 @@ def search_ont(sn: str, host: str) -> tuple[dict, str | None] | None:
 
         ont_info['catv'] = catv_results
         ont_info['eth'] = eth_results
+        del ont_info['_catv_ports']
 
         channel.close()
         ssh.close()
@@ -172,13 +173,13 @@ def read_output(channel: Channel, force: bool = True):
         #     print('no new data more than 1.5 seconds')
         #     break
         # if no new data more than 15 seconds and output is empty
-        if time() - last_data_time > 15 and len(output.strip().strip('\n').splitlines()) <= 4:
+        if time() - last_data_time > 15 and len(output.strip().strip('\n').splitlines()) <= 5:
             print('warn: no new data more than 15 seconds')
             print(output)
             break
         sleep(0.01)
 
-    return '\n'.join(output.splitlines()[1:]) if output.count('\n') > 1 else ''
+    return '\n'.join(output.splitlines()[1:]) if output.count('\n') > 1 else output
 
 def _parse_output(raw: str) -> tuple[dict, list[list[dict]]]:
     def _parse_value(value: str) -> str | float | int | bool | None:
@@ -322,11 +323,13 @@ def parse_basic_info(raw: str) -> dict:
             'seconds': int(uptime.group(4))
         } if uptime else None,
         '_catv_ports': next((item.get('Port-number') for item in ports_table or [] if item.get('Port-type') == 'CATV'), None),
-        '_eth_ports': next((item.get('Port-number') for item in ports_table or [] if item.get('Port-type') == 'ETH'), None)
+        # '_eth_ports': next((item.get('Port-number') for item in ports_table or [] if item.get('Port-type') == 'ETH'), None)
     }
 
 def parse_optical_info(raw: str) -> dict:
     """Parse ONT optical info"""
+    if 'The ONT is not online' in raw:
+        return {'status': 'fail', 'detail': 'ONT is not online'}
     data, _ = _parse_output(raw)
 
     return {
