1711b8b538f8775d23cb5acebf93a772bf595850
firedotguy <nta16022013@gmail.com>
2026-01-17 17:33:56 +0300

    fix(ont): return raw debug log

--- ont.py
+++ ont.py
@@ -421,7 +421,7 @@ def _parse_mac(raw: str) -> str | None:
         return
     raw = raw.replace('MAC TYPE', 'MAC-TYPE') # avoid extra spaces for better parsing (prefer "-")
     raw = raw.replace('It will take some time, please wait...', '') # remove extra text because it is near to table and can perceived as heading
-    print(_parse_output(raw)[1][0][0])
+    print(raw)
     return format_mac(_parse_output(raw)[1][0][0].get('MAC'))
 
 def _parse_onts_info(output: str) -> tuple[int, int, list[dict]] | tuple[dict, None, None]:
